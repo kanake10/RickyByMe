3bf13f8bf30c7ee3fb4935cace84f06dedd774fc
Kanake <10kanake@gmail.com>
2024-07-15 20:06:27 +0300

    Initial Commit

---
+++ Ricky/RickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyApp.swift
+//  Ricky
+//
+//  Created by Ezra Kanake on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
