75786cc45d3577d93c7dca6b9ea87a9630a5fbed
Kanake <10kanake@gmail.com>
2024-07-30 20:17:47 +0300

    first commit

---
+++ Ricky/models/Character.swift
@@ -0,0 +1,8 @@
+//
+//  Character.swift
+//  Ricky
+//
+//  Created by Ezra Kanake on 15/07/2024.
+//
+
+import Foundation
---
+++ Ricky/view/CharacterView.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterView.swift
+//  Ricky
+//
+//  Created by Ezra Kanake on 15/07/2024.
+//
+
+import Foundation
